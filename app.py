1cd244d74e31b047e771da2f174754251ed647f7
Mokoth <onyach.okoth1@outlook.com>
2023-06-19 17:18:53 +0300

    Add changes to files

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {'id': 1, 'title': 'Junior Flask developer', 'location': 'Nashvile', 'salary': 120000},
+  {'id': 2, 'title': 'Senior Python developer', 'location': 'Greenfield', 'salary': 100000},
+  {'id': 3, 'title': '.NET developer', 'location': 'Mombay', 'salary': 150000},
+  {'id': 4, 'title': 'FastAPI developer', 'location': 'Remote'},
+  {'id': 5, 'title': 'Fullstack developer', 'location': 'Brazil', 'salary': 220000}
+]
+
 @app.route('/')
 def index():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
+
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
